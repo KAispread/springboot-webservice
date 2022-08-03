b6e5fea51d949f18e49b076111d736f78350835f
KAispread <pain0121@naver.com>
2022-08-03 18:10:07 +0900

    test 수정

--- src/test/java/com/jojoldu/book/allears/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/allears/web/HelloControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,7 +24,8 @@ public class HelloControllerTest {
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
-        mvc.perform(get("/hello"))
+        mvc.perform(get("/hello")
+                        .accept(MediaType.TEXT_HTML))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
     }
