40e7c6f44e67352f02404fd2c9faf05f5c03afae
KAispread <pain0121@naver.com>
2022-08-03 20:54:45 +0900

    JpaRepository Test

---
+++ src/main/java/com/jojoldu/book/allears/domain/posts/PostRepository.java
@@ -0,0 +1,7 @@
+package com.jojoldu.book.allears.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Posts, Long>{
+
+}
