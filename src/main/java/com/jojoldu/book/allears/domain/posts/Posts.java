bb601a56de88bae25afe5c3723a8f1b6c79e84f6
KAispread <pain0121@naver.com>
2022-08-05 17:30:57 +0900

    저장, 수정, 조회 api 추가

--- src/main/java/com/jojoldu/book/allears/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/allears/domain/posts/Posts.java
@@ -3,14 +3,12 @@ package com.jojoldu.book.allears.domain.posts;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
 @Entity
 public class Posts {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
