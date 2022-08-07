f8823675d7d72f29e2b872d719f80d6f54d7c54d
KAispread <pain0121@naver.com>
2022-08-07 21:07:11 +0900

    수정 기능 추가

--- src/main/java/com/jojoldu/book/allears/web/IndexController.java
+++ src/main/java/com/jojoldu/book/allears/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.jojoldu.book.allears.web;
 
 import com.jojoldu.book.allears.service.posts.PostsService;
+import com.jojoldu.book.allears.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -17,9 +19,16 @@ public class IndexController {
         return "index";
     }
 
-
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("posts", dto);
+
+        return "posts-update";
     }
 }
